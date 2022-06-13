f679515eedf60a4f82084dd7b2992bf61982023e
Thomas <thomas.bogle@outlook.com>
2022-06-13 19:53:36 +0100

    Conditional rendering on SearchResults component

--- src/components/App.js
+++ src/components/App.js
@@ -5,14 +5,13 @@ import SearchResults from './SearchResults';
 
 const App = () => {
   const [searchResults, setSearchResults] = useState([]);
-  searchResults.map((e) => console.log(e));
 
   return (
     <div className="app">
       <img className="nasa-logo" src="https://cdn.cnn.com/cnnnext/dam/assets/200424060716-nasa-worm-logo.jpg" 
       alt="nasaLogo" />
       <Search setSearchResults={setSearchResults} />
-      <SearchResults searchResults={searchResults}/>
+      <SearchResults results={searchResults}/>
     </div>
   );
 }
--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -1,18 +1,23 @@
 import React from "react";
 import "../styles/searchresults.css";
 
-const SearchResults = () => {
-  return(
-    <>
-      <div className="search-results">
-        <p>Search Results</p>
-        <img className="card-name" 
-          src="https://images.unsplash.com/photo-1522030299830-16b8d3d049fe?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80" 
-          alt="space-pic" 
-        />
-      </div>
-    </>
-  );
+const SearchResults = ({ results }) => {
+  if(!results.length) {
+    return <p>No Results</p>
+  } else {
+
+    return(
+      <>
+        <div className="search-results">
+          <p>Search Results</p>
+          <img className="card-name" 
+            src="https://images.unsplash.com/photo-1522030299830-16b8d3d049fe?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80" 
+            alt="space-pic" 
+          />
+        </div>
+      </>
+    );
+  }
 };
 
 export default SearchResults;
