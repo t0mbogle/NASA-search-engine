2330142af37817d24a904725f4da8d8a58440851
Thomas <thomas.bogle@outlook.com>
2022-06-13 17:46:02 +0100

    Added SearchResults, no testing or styling

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../styles/app.css';
 import Search from './Search';
+import SearchResults from './SearchResults';
 
 const App = () => {
+  const [searchResults, setSearchResults] = useState();
+
   return (
     <div className="app">
-      <img src="https://cdn.cnn.com/cnnnext/dam/assets/200424060716-nasa-worm-logo.jpg" 
-      alt="nasaLogo" className="nasa-logo" />
-      <Search />
+      <img className="nasa-logo" src="https://cdn.cnn.com/cnnnext/dam/assets/200424060716-nasa-worm-logo.jpg" 
+      alt="nasaLogo" />
+      <Search setSearchResults={setSearchResults} />
+      <SearchResults />
     </div>
   );
 }
--- src/components/Search.js
+++ src/components/Search.js
@@ -2,19 +2,19 @@ import React, { useState } from "react";
 import "../styles/search.css";
 import getImages from "../requests/getImages";
 
-const Search = () => {
+const Search = ({ setSearchResults }) => {
   const [value, setValue] = useState();
 
   const handleSubmit = (event) => 
     event.preventDefault();
-    getImages(value);
+    setSearchResults(getImages(value));
 
     return(
         <>
         <form className="search-form" onSubmit={handleSubmit}>
           <input type="text" className="search-input" placeholder="search away" 
-            onChange={(e) => setValue(e.target.value)} />
-          <button type="submit" className="search-button">Search</button>
+            onChange={(event) => setValue(event.target.value)} />
+          <button type="submit" className="search-button">Go!</button>
           </form>
         </>
     );
